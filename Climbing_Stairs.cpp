820a4dd4380daaa9e1f54168885c15ead341f3d8
sajdakabir <sajda.kbir.com>
2022-03-03 19:44:57 +0530

    climbing stairs

--- Climbing_Stairs.cpp
+++ Climbing_Stairs.cpp
@@ -12,6 +12,8 @@ public:
         return ans;
     }
 };
+// Time complexity-->O(n) and 
+// space complexity-->O(n)for axuliry space + O(n) for dp array =O(2n) or O(n)
 
 
 // Tabulation
@@ -29,3 +31,29 @@ public:
         return dp[n];
     }
 };
+
+// Time complexity-->O(n) and 
+// space complexity-->O(n) (dp array)
+
+
+// Space Optimization
+class Solution {
+public:
+   
+    int climbStairs(int n) {
+      
+      int  prev2=1;
+        int prev=1;
+   
+        for(int i=2;i<=n;i++){
+            int curr_i=prev2+prev;
+            prev2=prev;
+            prev=curr_i;
+            
+        }
+        return prev;
+    }
+};
+
+// Time complexity-->O(n) and 
+// space complexity-->O(1) 
