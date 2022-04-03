7d5d4da465124d50d3d4602fbe7af2b04100223e
sajdakabir <sajda.kbir.com>
2022-04-04 00:44:00 +0530

    added total unique paths

--- Unique_paths.cpp
+++ Unique_paths.cpp
@@ -16,6 +16,29 @@ public:
     }
 };
 
+
+// sloution---->>2
+
+int countWaysUtil(int i, int j, vector<vector<int> > &dp) {
+  if(i==0 && j == 0)
+    return 1;
+  if(i<0 || j<0)
+    return 0;
+  if(dp[i][j]!=-1) return dp[i][j];
+    
+  int up = countWaysUtil(i-1,j,dp);
+  int left = countWaysUtil(i,j-1,dp);
+  
+  return dp[i][j] = up+left;
+  
+}
+
+int countWays(int m, int n){
+    vector<vector<int> > dp(m,vector<int>(n,-1));
+    return countWaysUtil(m-1,n-1,dp);
+    
+}
+
 // Time Complexity: O(M*N)
 // Reason: At max, there will be M*N calls of recursion.
 
@@ -51,6 +74,28 @@ public:
     }
 };
 
+// sloution --->>2
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){ 
+                    dp[i][j]= 1;
+                    continue;
+                                }
+                
+                int up=0;
+                if(i>0) up=dp[i-1][j];
+                int left=0;
+                if(j>0)left=dp[i][j-1];
+                dp[i][j]=up+left;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
 // Time Complexity: O(M*N)
 
 // Reason: There are two nested loops
