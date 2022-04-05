71fc8340bf045bb4b72839e557bafbde6f695673
sajdakabir <sajda.kbir.com>
2022-04-05 15:54:39 +0530

    new

--- Minimun_path_sum.cpp
+++ Minimun_path_sum.cpp
@@ -35,7 +35,35 @@ public:
 // Tabulation
 
 
-
+class Solution {
+public:
+    
+    int minPathSum(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+       for(int j=n-1;j>=0;j--){
+           for(int i=m-1;i>=0;i--){
+                if(i==m-1 && j==n-1){
+                    dp[i][j]=grid[i][j];
+                   
+                }
+               else{
+                int right=grid[i][j];
+               if(j<n-1)right+=dp[i][j+1];
+                   else right+=1e9;
+               int down=grid[i][j];
+               if(i<m-1) down+=dp[i+1][j];
+                   else down+=1e9;
+               
+               dp[i][j]=min(right,down);
+               }
+           }
+       }
+        return dp[0][0];
+        
+    }
+};
 
 
 
